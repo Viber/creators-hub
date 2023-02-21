30801f7e5be1cba9fffd22c4a45804f8bd0b639a
Or Chen <orchen@viber.com>
2023-02-21 17:48:26 +0200

    link our github to the site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Viber/creators-hub/edit/main/',
         },
         blog: false,
         theme: {
@@ -94,7 +94,7 @@ const config = {
             ]
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/Viber/creators-hub',
             label: 'GitHub',
             position: 'right',
           },
