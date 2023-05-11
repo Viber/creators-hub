fe8e51319a9465cac40d507beeb19e70f5c85dc8
eugene-zv <118444608+eugene-zv@users.noreply.github.com>
2023-05-11 14:12:13 +0400

    SD-2097 Update Support Button Styling in Navbar (#4)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -149,10 +149,9 @@ const config = {
             className: 'navbar-github-link',
           },
           {
-            href: 'https://help.viber.com',
-            label: 'Support',
-            target: '_blank',
+            type: 'html',
             position: 'right',
+            value: '<a href="https://help.viber.com" target="_blank" rel="noopener noreferrer" class="navbar-support-link">Support<svg style="margin-left: 4px" width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></a>',
           },
         ],
       },
